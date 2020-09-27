1f64da4cbf3812f84abba4d18fdd67d403068b05
gongxiao <1162193779@qq.com>
2020-09-27 10:25:43 +0800

    new branch

--- src/main/java/com/mysql/jdbc/Test.java
+++ src/main/java/com/mysql/jdbc/Test.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
+//新分支
 public class Test {
 
     public  static void main(String[] args)throws Exception {
